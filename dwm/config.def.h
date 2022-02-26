593b420a7029d7fbb941ab5730f3a07430813e71
Ishaan Kapoor <ishaankapoor2506@gmail.com>
2022-02-26 17:10:05 +0530

    Added Three Column Layout

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -46,9 +46,10 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "|||",      tcl },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[=]",      tile },
 };
 
 /* key definitions */
@@ -86,7 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
@@ -113,7 +114,7 @@ static Key keys[] = {
         { MODKEY,                       XK_F3,     spawn,          {.v = volup      } },
         { MODKEY,                       XK_F5,     spawn,          {.v = statbarref } },
         { MODKEY,                       XK_x,      movecenter,     {0} },
-
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[0]} },
 };
 
 /* button definitions */
