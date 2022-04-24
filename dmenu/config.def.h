7e064f42d6e3525fe628c745786fc3fd0f705110
Ishaan Kapoor <ishaankapoor2506@gmail.com>
2022-04-24 19:50:38 +0530

    Added fuzzy search patch to dmenu

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -1,9 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;                     /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                   /* -c option; centers dmenu on screen */
+static int min_width = 500;                /* minimum width when centered */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	" CaskaydiaCove Nerd Font:style=Book:size=14:antialias=true:autohint=true",
