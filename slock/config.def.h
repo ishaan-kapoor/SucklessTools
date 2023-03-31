c77dae0cffde1372d9fc501adbb010a56713db04
ishaan-kapoor <ishaankapoor2506@gmail.com>
2023-03-31 22:51:42 +0000

    Added Blur Background and DWM Logo Patch to Slock

--- slock/config.def.h
+++ slock/config.def.h
@@ -3,7 +3,8 @@ static const char *user  = "ishaan"; // "nobody"
 static const char *group = "ishaan"; // "nogroup"
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+    [BACKGROUND] =   "black",     /* after initialization */
+    [INIT] =   "#2d2d2d",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[INPUT_ALT] = "#227799", /* during input, second color */
 	[FAILED] = "#CC3333",   /* wrong password */
@@ -24,3 +25,33 @@ static const struct secretpass scom[entrylen] = {
 /*	 Password		    command */
 	// { "clean",   "/home/ishaan/programs/scripts/clean-screen" },
 };
+
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=10;
+/*Enable Pixelation*/
+#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=10;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;   /* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+       /* x    y       w       h */
+       { 0,    3,      1,      3 },
+       { 1,    3,      2,      1 },
+       { 0,    5,      8,      1 },
+       { 3,    0,      1,      5 },
+       { 5,    3,      1,      2 },
+       { 7,    3,      1,      2 },
+       { 8,    3,      4,      1 },
+       { 9,    4,      1,      2 },
+       { 11,   4,      1,      2 },
+
+};
+
